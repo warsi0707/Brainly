6b60f2d28094d2eee050b9b671fc768df45f05b9
Samir warsi <samir.warsi2001@gmail.com>
2025-10-05 19:25:40 +0530

    UI updated

--- frontend/src/Utils/BackendUrl.js
+++ frontend/src/Utils/BackendUrl.js
@@ -1,2 +1,2 @@
 // export const BackendUrl = 'http://localhost:3000/api/v1'
-export const BackendUrl = 'https://brainly-be-xavq.onrender.com'
+export const BackendUrl = 'https://brainly-be-xavq.onrender.com/api/v1'
